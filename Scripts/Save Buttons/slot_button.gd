56607e3db0036914b53f094ea9cacc97bf942b9a
Xiaoyi Chen <104607544@student.swin.edu.au>
2026-05-10 18:57:35 +1000

    created save system base
    simple system that creates save/load files in player folder

---
+++ Scripts/Save Buttons/slot_button.gd
@@ -0,0 +1,25 @@
+extends Button
+
+@export var slot_number := 1
+
+func _ready():
+	update_button_text()
+	pressed.connect(_on_pressed)
+
+
+func update_button_text():
+	if SaveManager.save_exists(slot_number):
+		text = "Load Slot %d" % slot_number
+	else:
+		text = "New Game Slot %d" % slot_number
+
+
+func _on_pressed():
+	if SaveManager.save_exists(slot_number):
+		var data = SaveManager.load_game(slot_number)
+		print("Loaded data: ", data)
+
+		get_tree().change_scene_to_file(data["current_scene"])
+	else:
+		SaveManager.create_new_save(slot_number)
+		get_tree().change_scene_to_file("res://Scenes/MainGame.tscn")
--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -17,3 +17,7 @@ func _on_settings_button_pressed() -> void:
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/save_slots.tscn")
